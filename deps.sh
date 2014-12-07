8eff978770bab3143f145563c8262719b4e68903
eva <eva.tallaksen@gmail.com>
2014-12-07 12:09:12 +0000

    add uv4l sources

--- deps.sh
+++ deps.sh
@@ -1,2 +1,4 @@
+wget http://www.linux-projects.org/listing/uv4l_repo/lrkey.asc && apt-key add ./lrkey.ascapt-get -y update
+echo "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/ wheezy main" | tee -a /etc/apt/sources.list
 apt-get -y update
 apt-get -y install uv4l uv4l-raspicam uv4l-raspicam-extras
