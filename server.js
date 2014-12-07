8ced79f7a1ffffb5d70cd02f78f6a3c34b14689e
eva <eva.tallaksen@gmail.com>
2014-12-07 14:07:43 +0000

    missing bracket

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var camera, createCamera;
 
 var fs = require('fs');
-if (fs.existsSync('/tmp/snowsnap/snapshot.jpg'){
+if (fs.existsSync('/tmp/snowsnap/snapshot.jpg')){
 	fs.unlinkSync('/tmp/snowsnap/snapshot.jpg');
 };
 
