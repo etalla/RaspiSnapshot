28a7b6626bb80f208ea16d165c0ba46eb63eab79
eva <eva.tallaksen@gmail.com>
2014-12-07 10:33:37 +0000

    creates photo taking function

--- takephoto.js
+++ takephoto.js
@@ -1,24 +1,28 @@
 var RaspiCam = require("raspicam");
 
-var camera = new RaspiCam({ opts });
+var photo = new RaspiCam({ 
+	mode: "photo",
+	output: "/data/snap.jpg", // String - the path and filename where you want to store the photos (use sprintf-style variables, like %d, for incrementing timelapse photos)
+	encoding: "jpg"
+});
 
 //to take a snapshot, start a timelapse or video recording
-camera.start( );
+photo.start( );
 
 //to stop a timelapse or video recording
-camera.stop( );
+photo.stop( );
 
 //listen for the "started" event triggered when the start method has been successfully initiated
-camera.on("started", function(){ 
-    //do stuff
+photo.on("started", function(){ 
+    console.log("started");
 });
 
 //listen for the "read" event triggered when each new photo/video is saved
-camera.on("read", function(err, timestamp, filename){ 
-    //do stuff
+photo.on("read", function(err, timestamp, filename){ 
+    console.log("read");
 });
 
 //listen for the process to exit when the timeout has been reached
-camera.on("exited", function(){
-    //do stuff
+photo.on("exited", function(){
+    console.log("exited");
 });
