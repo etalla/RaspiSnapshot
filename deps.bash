dbd59e9287d4a79d4fdaa24ff800f93cf63319d4
eva <eva.tallaksen@gmail.com>
2014-12-07 10:48:53 +0000

    adds dept.bash

---
+++ deps.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/raspberrypi/userland && \
+	apt-get -y update && \
+	apt-get -y install cmake curl && \
+	pushd userland && \
+	mkdir build && \
+	pushd build && \
+	cmake -DCMAKE_BUILD_TYPE=Release .. && \
+	make && \
+	cp bin/* /usr/bin/ && \
+	cp lib/* /usr/lib/ && \
+	popd && \
+	popd
